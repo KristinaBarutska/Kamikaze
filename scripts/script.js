40017a047bc061b0928a7118537528b04f17ba11
Росица Бъндева <rossy@compass-bg.net>
2016-08-11 01:54:21 +0300

    Resize images

--- scripts/script.js
+++ scripts/script.js
@@ -1,23 +1,23 @@
 var Source = "#boxcard";
 
 var ImgSource = [
-  "../img/1.jpg",
-  "../img/2.jpg",
-  "../img/3.jpg",
-  "../img/4.jpg",
-  "../img/5.jpg",
-  "../img/6.jpg",
-  "../img/7.jpg",
-  "../img/8.jpg",
-  "../img/9.jpg",
-  "../img/10.jpg"
+  '../img/1.jpg',
+  '../img/2.jpg',
+  '../img/3.jpg',
+  '../img/4.jpg',
+  '../img/5.jpg',
+  '../img/6.jpg',
+  '../img/7.jpg',
+  '../img/8.jpg',
+  '../img/9.jpg',
+  '../img/10.jpg'
 ];
 
 $(function() {
 
 for (var i = 1; i < 3 ; i++) {
 	$.each(ImgSource, function(i, val) {
-		$(Source).append("<div " + i + 1 + "/> ");
+		$(Source).append('<div />');
 	});
 }
 }); 
