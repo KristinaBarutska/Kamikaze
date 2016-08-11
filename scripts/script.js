015b65a079a92c230c2454c85c5129c22fb47a8a
Georgi Karaboichev <djorkaevhell@abv.bg>
2016-08-11 17:31:47 +0300

    Update Clicking

--- scripts/script.js
+++ scripts/script.js
@@ -11,6 +11,7 @@ var Tile = function (x, y, face) {
     this.y = y;
     this.face = face;
     this.width = 100;
+    this.isFaceUp = false;
 };
 
 var images = [
@@ -34,16 +35,14 @@ Tile.prototype.drawFaceDown = function () {
     ctx.fillRect(this.x, this.y, this.width, this.width);
     ctx.stroke();
     ctx.drawImage(backImage, 0, 0, this.width, this.width, this.x, this.y, this.width, this.width);
-    this.isFaceUp = false;
 };
 
 Tile.prototype.drawFaceUp = function () {
-    tx.fillStyle = '#6991AC';
+    ctx.fillStyle = '#6991AC';
     ctx.strokeStyle = "#999";
     ctx.fillRect(this.x, this.y, this.width, this.width);
     ctx.stroke();
-    ctx.drawImage(backImage, 0, 0, this.width, this.width, this.x, this.y, this.width, this.width);
-    this.isFaceUp = true;
+    ctx.drawImage(this.face, 0, 0, this.width, this.width, this.x, this.y, this.width, this.width);
 };
 
 Tile.prototype.isUnderMouse = function (x, y) {
@@ -54,7 +53,7 @@ Tile.prototype.isUnderMouse = function (x, y) {
 var possibleFaces = images.slice(0);
 var selected = [];
 for (var i = 0; i < (cols * rows) / 2; i++) {
-    var randomInd = Math.floor(Math.random(possibleFaces.length));
+    var randomInd = Math.floor(Math.random() * possibleFaces.length);
     var face = possibleFaces[randomInd];
     selected.push(face);
     selected.push(face);
@@ -69,30 +68,88 @@ selected.sort(function () {
 var tiles = [];
 for (var col = 0; col < cols; col++) {
     for (var row = 0; row < rows; row++) {
-        tiles.push(new Tile(col * 110 + 40 , row * 110, selected.pop()))
+        tiles.push(new Tile(col * 110 + 40 , row * 110, undefined));
+        console.log(tiles.face);
     }
-}
-
-// Now draw them face up
-for (var i = 0; i < tiles.length; i++) {
-    tiles[i].drawFaceDown();
 }
 
 var flippedTiles = [];
 var delayStartFC = null;
 var numTries = 0;
-
+var randomImageIndex = 0;
+var clickedCount = 0;
+var firstPreviousClickedTile = 0;
+var secondPreviousClickedTile = 0;
 canvas.addEventListener('click', function (event) {
     var x = event.pageX - canvasLeft,
         y = event.pageY - canvasTop;
     console.log(x, y);
-    tiles.forEach(function (element) {
-        if (y > element.y && y < element.y + element.width && x > element.x && x < element.x + element.width) {
-            console.log('clicked an element '+ x + ' ' + y);
-        }
-    });
 
-}, false)
+    for (var i = 0; i < tiles.length; i++) {
+        var tile = tiles[i];
+        if (tile.isUnderMouse(x, y)) {
+            tile.isFaceUp = true;
+            
+            if(tile.face === undefined){
+                var hasToBreak = false;
+                while(!hasToBreak){
+                    randomImageIndex = Math.floor(Math.random() * tiles.length / 2);
+                    var current = numberStates[randomImageIndex];
+                    hasToBreak = false;
+                    if (!current.state) {
+                        current.count += 1;
+                        if(current.count == 3){
+                            current.state = true;
+                            continue;
+                        }
+                        hasToBreak = true;
+                    }
+                }
+                tile.face = images[randomImageIndex];
+            }
+            	
+            console.log('clicked an element '+ x + ' ' + y);
+            clickedCount += 1;
+            if(clickedCount % 2 == 1){
+                if(clickedCount !== 1 && (firstPreviousClickedTile.face !== secondPreviousClickedTile.face)){
+                    firstPreviousClickedTile.isFaceUp = false;
+                    secondPreviousClickedTile.isFaceUp = false;
+                }       
+                firstPreviousClickedTile = tile;            
+            }
+            else{
+                secondPreviousClickedTile = tile;
+            }
+            Draw();
+        }
+        
+    }
+
+}, false);
+
+
+var numberStates = [];
+for (var i = 0; i < tiles.length / 2; i++) {
+    numberStates.push({
+        state: false,
+        count: 0
+    });
+}
+
+// Draw
+function Draw(){
+
+    for (var i = 0; i < tiles.length; i++) {
+        if (!tiles[i].isFaceUp) {
+            tiles[i].drawFaceDown();
+            continue;
+        }
+
+        tiles[i].drawFaceUp();
+    }
+}
+
+Draw();
 
 console.log(tiles);
     
